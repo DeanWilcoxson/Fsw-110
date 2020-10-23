6195719e2789c7181d615bfa9b1566a7b8599363
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-10-22 22:39:28 -0700

    10/22/20.1

--- Week1/document_object/index.js
+++ Week1/document_object/index.js
@@ -1,8 +1,1 @@
-var myObject = {
-    firstName: "Dean",
-    lastName: "Wilcoxson",
-    preferredNickName: "Deanoe",
-    age: 29,
-    hobbyList: ["coding", "games", "sports", "rapping", "family-time", ]
-};
 console.log(document);
