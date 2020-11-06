15d806d3b458ad7d0193d864c5f6b947d4e0f939
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-05 16:49:57 -0800

    11/5/20.1

--- Week3/project1/index.js
+++ Week3/project1/index.js
@@ -1,3 +1,19 @@
-var newH1Item = document.createElement("h1");
-newH1Item.textContent = "Hello World";
-document.body.append(newH1Item);
+//step4.Create a for loop that repeats the process ten times 
+for (i = 0; i < 10; i++) {
+
+    //step1.create an <h1></h1> Element
+    var newH1Element = document.createElement("h1");
+
+    //step2.Give it Content and Style
+    newH1Element.textContent = "Hello World";
+    newH1Element.style.color = "blue";
+    newH1Element.style.backgroundColor = "darkgrey";
+    newH1Element.style.textAlign = "center";
+
+    //step3.Append it to the body
+    document.body.append(newH1Element);
+}
+var names = ["steve", "larry", "joe", "shirley", "steph", "nate", "rick", "emily"];
+for (i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
