dbfc348b9296f4311953701730a83e63b16307a4
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-11 06:24:06 -0800

    11/11/20.6

--- Week4/Project 1/Logic.js
+++ Week4/Project 1/Logic.js
@@ -1,20 +1,27 @@
 //Body Background Color
 document.body.style.backgroundColor = "darkgrey";
+
 //Background color of squares
 //What to do when the "mouseout" event happens
 function regularBackground() {
     myHoverSquare.style.backgroundColor = "transparent";
     myHoverSquare.style.color = "black";
+
     myClickSquare.style.backgroundColor = "transparent";
     myClickSquare.style.color = "black";
+    myClickSquare.style.border = "5px solid red";
+
     myDblClickSquare.style.backgroundColor = "transparent";
     myDblClickSquare.style.color = "black";
+
     myScrollSquare.style.backgroundColor = "transparent";
     myScrollSquare.style.color = "black";
 }
+
 //Create the event listeners for "hover"
 var myHoverSquare = document.getElementById("hover");
 myHoverSquare.addEventListener("mouseover", hoverColor);
+
 //What to do when the "mouseover" happens
 function hoverColor() {
     myHoverSquare.style.backgroundColor = "blue";
@@ -25,27 +32,34 @@ function hoverColor() {
 var myClickSquare = document.getElementById("click");
 myClickSquare.addEventListener("mousedown", downColor);
 myClickSquare.addEventListener("mouseup", upColor);
+
 //What to do when the "mousedown" happens
 function downColor() {
     myClickSquare.style.backgroundColor = "red";
     myClickSquare.style.color = "white";
 }
+
 //What to do when the "mouseup" happens
 function upColor() {
     myClickSquare.style.backgroundColor = "yellow";
     myClickSquare.style.color = "black";
+    myClickSquare.style.border = "5px solid yellow";
 }
+
 //Create the event listeners for "dbl-click"
 var myDblClickSquare = document.getElementById("dbl-click");
 myDblClickSquare.addEventListener("dblclick", dblClickColor);
+
 //What to do when the "dblclick" happens
 function dblClickColor() {
     myDblClickSquare.style.backgroundColor = "green";
     myDblClickSquare.style.color = "white";
 }
+
 //Create the event listeners for "scroll"
 var myScrollSquare = document.getElementById("scroll");
 document.addEventListener("wheel", scrollColor);
+
 //What to do when the "wheel" happens
 function scrollColor() {
     myScrollSquare.style.backgroundColor = "orange";
