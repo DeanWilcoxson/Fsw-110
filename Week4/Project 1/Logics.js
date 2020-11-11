c49d16bec7cc85a01bd33e75897edff0380feb99
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-11 01:40:08 -0800

    11/11/20

---
+++ Week4/Project 1/Logics.js
@@ -0,0 +1,48 @@
+document.body.style.backgroundColor = "darkgrey";
+
+function regularBackground() {
+    myHoverSquare.style.backgroundColor = "transparent";
+    myHoverSquare.style.color = "black";
+    myClickSquare.style.backgroundColor = "transparent";
+    myClickSquare.style.color = "black";
+    myDblClickSquare.style.backgroundColor = "transparent";
+    myDblClickSquare.style.color = "black";
+    myScrollSquare.style.backgroundColor = "transparent";
+    myScrollSquare.style.color = "black";
+}
+
+var myHoverSquare = document.getElementById("hover");
+myHoverSquare.addEventListener("mouseover", hoverColor);
+
+function hoverColor() {
+    myHoverSquare.style.backgroundColor = "blue";
+    myHoverSquare.style.color = "white";
+}
+
+var myClickSquare = document.getElementById("click");
+myClickSquare.addEventListener("mousedown", downColor);
+myClickSquare.addEventListener("mouseup", upColor);
+
+function downColor() {
+    myClickSquare.style.backgroundColor = "red";
+}
+
+function upColor() {
+    myClickSquare.style.backgroundColor = "yellow";
+    myClickSquare.style.color = "black";
+}
+
+var myDblClickSquare = document.getElementById("dbl-click");
+myDblClickSquare.addEventListener("dblclick", dblClickColor);
+
+function dblClickColor() {
+    myDblClickSquare.style.backgroundColor = "green";
+    myDblClickSquare.style.color = "white";
+}
+
+var myScrollSquare = document.getElementById("scroll");
+document.addEventListener("wheel", scrollColor);
+
+function scrollColor() {
+    myScrollSquare.style.backgroundColor = "orange";
+}
