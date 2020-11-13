946e93783a49737e8a4d3e263d6250682b072e55
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-13 03:48:49 -0800

    11/12/20.8

--- Week4/Project 2/Logic.js
+++ Week4/Project 2/Logic.js
@@ -1,8 +1,30 @@
-var firstAddition = document.getElementById("add1").value;
-var secondAddition = document.getElementById("add2").value;
-num1add = firstAddition;
-num2add = secondAddition;
-
-function additionCalc(num1add, num2add) {
-
+//Addition Block
+function addTwo(num1, num2) {
+    var sum = +num1 + +num2;
+    return (sum);
 }
+var addBtn = document.getElementById("addbtn");
+addBtn.addEventListener("click", function() {
+    var num1 = document.getElementById("add1").value;
+    var num2 = document.getElementById("add2").value;
+    var addCalc = document.getElementById("calculation");
+    var sum = addTwo(num1, num2);
+    addCalc.textContent = sum;
+});
+
+//Subtraction Block
+function subTwo(num1, num2) {
+    var result = num1 - num2;
+    return (result);
+}
+var subBtn = document.getElementById("subbtn");
+subBtn.addEventListener("click", function() {
+    var num1 = document.getElementById("sub1").value;
+    var num2 = document.getElementById("sub2").value;
+    var subCalc = document.getElementById("calculation");
+    var result = subTwo(num1, num2);
+    subCalc.textContent = result;
+});
+
+
+//Multiplication Block
