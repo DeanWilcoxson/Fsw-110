68869f2b8c982546aee2c81a738e92c592eb57d2
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-13 03:48:23 -0800

    11/12/20.7

---
+++ Week4/Project 2/Logic.js
@@ -0,0 +1,8 @@
+var firstAddition = document.getElementById("add1").value;
+var secondAddition = document.getElementById("add2").value;
+num1add = firstAddition;
+num2add = secondAddition;
+
+function additionCalc(num1add, num2add) {
+
+}
