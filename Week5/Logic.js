0f7cce10cd6b5a85693a6b292e039ad4f964e664
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-18 14:55:26 -0800

    11/18/20

---
+++ Week5/Logic.js
@@ -0,0 +1,31 @@
+var form = document.getElementById("FormID");
+form.style.backgroundColor = "grey";
+form.style.display = "flex";
+form.style.flexDirection = "column";
+form.style.justifyContent = "center";
+form.style.alignItems = "center";
+form.style.border = "3px solid blue";
+form.style.borderRadius = "5px";
+form.style.boxShadow = "5px 5px 5px rgba(0, 0, 5, 0.6)";
+form.style.padding = "10px";
+
+var username = document.getElementById("name");
+username.style.margin = "20px";
+username.style.display = "flex";
+username.style.flexDirection = "row";
+username.style.boxShadow = "5px 5px 5px rgba(0, 0, 5, 0.6)";
+
+var password = document.getElementById("password");
+password.style.margin = "20px";
+password.style.display = "flex";
+password.style.flexDirection = "row";
+password.style.boxShadow = "5px 5px 5px rgba(0, 0, 5, 0.6)";
+
+var passwordConfirm = document.getElementById("confirm");
+passwordConfirm.style.margin = "20px";
+passwordConfirm.style.display = "flex";
+passwordConfirm.style.flexDirection = "row";
+passwordConfirm.style.boxShadow = "5px 5px 5px rgba(0, 0, 5, 0.6)";
+
+var accountCreated = document.getElementById("button");
+accountCreated.style.boxShadow = "5px 5px 5px rgba(0, 0, 5, 0.6)";
