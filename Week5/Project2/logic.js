4ab2dd715d681667946f3aaa6b13318e3ef3e178
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-21 17:54:36 -0800

    11/21/20.4

--- Week5/Project2/logic.js
+++ Week5/Project2/logic.js
@@ -1,4 +1,17 @@
 /*jshint esversion: 6 */
-const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName");
-const age = document.getElementById("ageNum");
+document.getElementById("submit").addEventListener("click", function() {
+    const firstName = document.getElementById("firstName");
+    const lastName = document.getElementById("lastName");
+    const age = document.getElementById("ageNum");
+    const location = document.getElementById("location");
+    const male = document.getElementById("genderMale");
+    const female = document.getElementById("genderFemale");
+    alert(`First Name:${firstName.value} \nLast Name:${lastName.value} \nAge:${age.value} \nLocation:${location.value}`);
+});
+
+
+function manOrWoman() {
+    if (male.checked === true) {
+        return male;
+    }
+}
