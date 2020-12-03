0be2b56a318bb4b3dd14f27991ff14455dae63ae
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-12-03 02:10:36 -0800

    12/3/20

--- Week7/CapstoneTODO.js
+++ Week7/CapstoneTODO.js
@@ -1,10 +1,54 @@
-var title = document.getElementById("title");
-var description = document.getElementById("desc");
-var buttonVar = document.getElementById("buttonSub");
-var toDoList = document.getElementById("toDo");
-buttonVar.addEventListener("click", function(e) {
-    var listItem = document.createElement("li");
-    toDoList.append(listItem);
-    listItem.textContent = "Task: " + title.value + " = " + description.value;
-    form.reset();
-});
+/* jshint esversion: 6 */
+//Selecting Form Elements with Javascript.
+let title = document.getElementById("title");
+let description = document.getElementById("description");
+let buttonVar = document.getElementById("buttonSub");
+let toDoList = document.getElementById("toDo");
+let formVar = document.getElementById("form");
+
+//Create an Empty Array for the List Items to be pushed into, for calling them after created.
+let listedTasks = [];
+
+//Add Tasks to the List.
+function addListItem() {
+    if ((title.value != "") && (description.value != "")) {
+
+        //List Item.
+        let listItem = document.createElement("li");
+        listItem.style.color = "blue";
+        listItem.style.border = "3px solid rgba(172, 255, 47, 0.575)";
+        listItem.style.backgroundColor = "honeydew";
+        listItem.textContent = "Task: " + title.value + " === " + description.value;
+        toDoList.append(listItem);
+
+        //Delete Button.
+        let deleteListItem = document.createElement("button");
+        deleteListItem.classList = "Delete";
+        deleteListItem.style.color = "white";
+        deleteListItem.style.backgroundColor = "blue";
+        deleteListItem.style.margin = "20px";
+        deleteListItem.textContent = "Delete";
+        listItem.append(deleteListItem);
+
+        //Push created Item to the List Array.
+        listedTasks.push(listItem);
+
+        //Reset Form for next Task to be created.
+        form.reset();
+
+        //Alert the user they have not filled out the form.
+    } else(alert("Please enter a value for BOTH fields. Thank You!"));
+}
+//When the user clicks run this Function.
+buttonVar.addEventListener("click", addListItem);
+
+//Delete list Items from the To-Do List.
+/* toDoList.addEventListener("click", function(e) {
+    let deleteButton = document.getElementsByClassName("Delete");
+    for (i = 0, i < deleteButton.length; i++;) {
+        if (e.target == deleteButton[i]) {
+            let deleted = deleteButton[i];
+            toDoList.removeChild(deleted);
+        }
+    }
+}); */
